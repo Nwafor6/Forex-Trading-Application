bdc5c23a87212f859efc4864eaebc6fdeaa6dc3d
Nwafor6 <nwaforglory6@gmail.com>
2023-03-30 04:43:45 +0100

    Added secret key to .env file

--- trading/settings.py
+++ trading/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^n-5jb%=tsc+bj8b78nn8t*okd6tysh+hr0^^kh3_!2w(k!r&f'
-
+SECRET_KEY = config('DJANGO_SECRET_KEY',default='^4-737n**(f#lssg_(7zav%dg7@k=$76!ry%zbdrr$&@qrqddn')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
